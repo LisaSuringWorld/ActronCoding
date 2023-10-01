bc0828d20cd2247633a828f5ec7f02fbdbd2fd62
Lisa <foiclh@foxmail.com>
2023-10-01 12:26:05 +1100

    Improve solution using async Task and add definition for request and response body

--- ActronCoding.Tests/FormLargestIntFunctionTests.cs
+++ ActronCoding.Tests/FormLargestIntFunctionTests.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Http.Internal;
+using System.Threading.Tasks;
+using ActronCoding;
+using Moq;
 
 namespace ActronCoding.Tests
 {
@@ -16,7 +19,7 @@ namespace ActronCoding.Tests
         }
 
         [Test]
-        public void RunFunction_ReturnsOkResult()
+        public async Task RunFunction_ReturnsOkResult()
         {
             // Arrange
             var input = new InputModel { Input = new int[] { 10, 223, 78, 90, 99 } };
@@ -25,31 +28,39 @@ namespace ActronCoding.Tests
             {
                 Body = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(requestBody))
             };
-            var logger = TestFactory.CreateLogger();
+            //var logger = TestFactory.CreateLogger();
 
             // Act
-            var response = FormLargestIntFunction.Run(request, logger);
+            var logger = Mock.Of<ILogger<FormLargestIntFunction>>(); // Use a mocking framework like Moq or use Mock.Of
+
+            var function = new FormLargestIntFunction(logger);
+            var response = await function.Run(request, logger);
 
             // Log response content for debugging
             /*var responseContent = response != null ? response.Value?.ToString() : "Response is null";
             logger.LogInformation("Response content: " + responseContent);*/
 
             //var result = (OkObjectResult)response;
-            var result = response as JsonResult;
+            //var result = response as JsonResult;
+            var result = (JsonResult)response;
 
             // Assert
             Assert.IsNotNull(result);
             Assert.AreEqual(StatusCodes.Status200OK, result.StatusCode);
-    
+
+            var outputModel = (OutputModel)result.Value;
+            Assert.IsNotNull(outputModel);
+            Assert.AreEqual("99907822310", outputModel.output);
+
             // Parse the response content
-            var responseContent = JsonConvert.DeserializeObject<ResponseModel>(result.Value.ToString());
+            /*var responseContent = JsonConvert.DeserializeObject<ResponseModel>(result.Value.ToString());
 
             Assert.IsNotNull(responseContent);
-            Assert.AreEqual("99907822310", responseContent.output);
+            Assert.AreEqual("99907822310", responseContent.output);*/
         }
 
         [Test]
-        public void RunFunction_InvalidInput_ReturnsBadRequest()
+        public async Task RunFunction_InvalidInput_ReturnsBadRequest()
         {
             // Arrange
             var input = new InputModel { Input = new int[] { } }; // Invalid input with empty array
@@ -58,10 +69,14 @@ namespace ActronCoding.Tests
             {
                 Body = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(requestBody))
             };
-            var logger = TestFactory.CreateLogger();
+            //var logger = TestFactory.CreateLogger();
 
             // Act
-            var response = FormLargestIntFunction.Run(request, logger);
+            var logger = Mock.Of<ILogger<FormLargestIntFunction>>(); // Use a mocking framework like Moq or use Mock.Of
+
+            var function = new FormLargestIntFunction(logger);
+            var response = await function.Run(request, logger);
+
             var result = response as BadRequestObjectResult;
 
             // Assert
@@ -72,13 +87,13 @@ namespace ActronCoding.Tests
     }
 
    
-    public class ResponseModel
+    /*public class ResponseModel
     {
         public string output { get; set; }
-    }
+    }*/
 
     // Helper class to create a logger
-    public static class TestFactory
+    /*public static class TestFactory
     {
         public static ILogger CreateLogger()
         {
@@ -88,5 +103,5 @@ namespace ActronCoding.Tests
             });
             return loggerFactory.CreateLogger("TestLogger");
         }
-    }
+    }*/
 }
--- ActronCoding/FormLargestIntFunction.cs
+++ ActronCoding/FormLargestIntFunction.cs
@@ -15,7 +15,7 @@ using Newtonsoft.Json;
 
 namespace ActronCoding
 {
-    /*public class FormLargestIntFunction
+    public class FormLargestIntFunction
     {
         private readonly ILogger<FormLargestIntFunction> _logger;
 
@@ -24,31 +24,73 @@ namespace ActronCoding
             _logger = log;
         }
 
-        [FunctionName("Function1")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [FunctionName("FormLargestInt")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "Actron Coding Challenge" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        //[OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        //[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(InputModel), Required = true, Description = "Input array of integers")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(OutputModel), Description = "Largest value")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "Invalid input array [non positive integers or empty array]")]
+
+        //public IActionResult Run(
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "formLargestInt")] HttpRequest req,
+            ILogger log)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            //string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var input = JsonConvert.DeserializeObject<InputModel>(requestBody);
+
+            if (input == null || input.Input == null || input.Input.Length == 0 || input.Input.Any(x => x <= 0))
+            {
+                return new BadRequestObjectResult("Invalid input array [non positive integers or empty array]");
+            }
+
+            // Sort the input array to form the largest integer
+            Array.Sort(input.Input, (a, b) =>
+            {
+                string order1 = a.ToString() + b.ToString();
+                string order2 = b.ToString() + a.ToString();
+                return order2.CompareTo(order1);
+            });
+
+            var largestInt = string.Join("", input.Input);
+
+            // Return JSON response
+            /*var jsonResponse = JsonConvert.SerializeObject(new { output = largestInt });
+            return new JsonResult(jsonResponse)
+            {
+                //Value = jsonResponse,
+                ContentType = "application/json",
+                StatusCode = 200
+            };*/
+
+            // Return JSON response
+            return new JsonResult(new OutputModel { output = largestInt })
+            {
+                ContentType = "application/json",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+            /*dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
-        }
-    }*/
+            return new OkObjectResult(responseMessage);*/
 
-    public static class FormLargestIntFunction
+
+        }
+    }
+
+    /*public static class FormLargestIntFunction
     {
         [FunctionName("FormLargestInt")]
         public static IActionResult Run(
@@ -66,6 +108,7 @@ namespace ActronCoding
                 return new BadRequestObjectResult("Invalid input array [non positive integers or empty array]");
             }
 
+            // Sort the input array to form the largest integer
             Array.Sort(input.Input, (a, b) =>
             {
                 string order1 = a.ToString() + b.ToString();
@@ -75,11 +118,11 @@ namespace ActronCoding
 
             var largestInt = string.Join("", input.Input);
             //return new OkObjectResult(new { output = largestInt });
-            /*return new JsonResult(new { output = largestInt })
-            {
-                StatusCode = 200,
-                ContentType = "application/json"
-            };*/
+            //return new JsonResult(new { output = largestInt })
+            //{
+            //    StatusCode = 200,
+            //    ContentType = "application/json"
+            //};
 
             // Return JSON response
             var jsonResponse = JsonConvert.SerializeObject(new { output = largestInt });
@@ -90,11 +133,16 @@ namespace ActronCoding
                 StatusCode = 200
             };
         }
-    }
+    }*/
 
     public class InputModel
     {
         public int[] Input { get; set; }
+    }
+
+    public class OutputModel
+    {
+        public string output { get; set; }
     }
 }
 
